8df1cf310e19f8e510af78e3bc4df23c956a301f
Richard Wardin <richardw@hesonline.com>
2017-05-23 21:04:46 -0400

    OR works

--- app/models/program.rb
+++ app/models/program.rb
@@ -9,16 +9,50 @@ class Program < ApplicationRecord
   def evaluate_component(json_component)
     # What are we looking at?
     if json_component.is_a?(Hash)
-      # Ok we need to see what the key is:
+      evaluate_hash(json_component)
     elsif json_component.is_a?(Array)
-      # If it's an array, it's fine if there's only 1 thing:
-      if json_component.size == 1
-        return json_component[0]
-      else
-        raise "Cannot evaluate an array with length > 0: #{json_component.inspect}"
-      end
+      evaluate_array(json_component)
+    elsif json_component.is_a?(TrueClass) || json_component.is_a?(FalseClass)
+      return json_component
     else
       raise "Cannot evaluate unknown component: #{json_component.inspect}"
     end
   end
+
+  def evaluate_array(json_component)
+    # If it's an array, it's fine if there's only 1 thing:
+    if json_component.size == 1
+      return evaluate_component(json_component[0])
+    else
+      raise "Cannot evaluate an array with length > 0: #{json_component.inspect}"
+    end
+  end
+
+  def evaluate_hash(json_component)
+    # There should only be one key/value:
+    if json_component.keys.size == 1
+      key = json_component.keys.first
+      value = json_component[key]
+      # Ok we need to see what the key is:
+      if key == "or"
+        return evaluate_or(value)
+      else
+        raise "Unknown function '#{key}': #{json_component.inspect}"
+      end
+    else
+      raise "Cannot evaluate a hash with more than one key: #{json_component.inspect}"
+    end
+  end
+
+  def evaluate_or(params)
+    # params should be an array with exactly 2 elements:
+    if !params.is_a?(Array)
+      raise "parameters of 'or' must be an array: #{sides}"
+    end
+    if params.size != 2
+      raise "parameters of 'or' must contain exactly 2 elements: #{sides}"
+    end
+    return (evaluate_component(params[0]) or evaluate_component(params[1]))
+  end
+
 end
--- test/models/program_test.rb
+++ test/models/program_test.rb
@@ -1,15 +1,29 @@
 require 'test_helper'
 
 class ProgramTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # CONSTANTS
   test "constant true evaluates to true" do
     assert_equal true, programs(:constant_true).get_position(0)
   end
-
   test "constant false evaluates to false" do
     assert_equal false, programs(:constant_false).get_position(0)
   end
+  # IF-THEN-ELSE
+  # TODO: Write tests
+  # OR
+  test "false||false evaluates to false" do
+    assert_equal false, programs(:or_both_false).get_position(0)
+  end
+  test "true||false evaluates to true" do
+    assert_equal true, programs(:or_first_true).get_position(0)
+  end
+  test "false||true evaluates to true" do
+    assert_equal true, programs(:or_second_true).get_position(0)
+  end
+  test "true||true evaluates to true" do
+    assert_equal true, programs(:or_both_true).get_position(0)
+  end
+
+
 
 end
